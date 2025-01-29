5d40e06c9661227b918f7ddb87b63c00f575ce8b
nateusse <natyeusse@gmail.com>
2025-01-29 12:34:37 -0500

    solved exercsises

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -7,11 +7,11 @@ public class MenuBuilder {
 
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
-
+    String menuTitle = "My Dream Menu:";
     // Print the menuTitle variable to the console.
-
+    System.out.println(menuTitle);
     // Create a variable called menu of type ArrayList.
-
+    ArrayList<MenuItem> menu = new ArrayList<>();
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
 
--- _02_05b/Ticket.java
+++ _02_05b/Ticket.java
@@ -12,8 +12,30 @@ public class Ticket {
 
   // Add three public methods to set the value of each field, called
   // setDestination, setPrice and setIsReturn.
+  public void setDestination(String destino) {
+    this.destination = destino;
+  }
 
+  public void setPrice(double precio) {
+    this.price = precio;
+  }
+
+  public void setIsReturn(boolean returned) {
+    this.isReturn = returned;
+  }
   // Add three public methods to get the value of each field, called
   // getDestination, getPrice and getIsReturn.
+
+  public String getDestination() {
+    return this.destination;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public boolean getReturn() {
+    return this.isReturn;
+  }
 
 }
